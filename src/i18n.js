998198878b3405201288c2f3cb5db8a38711b9cb
Hazem <poppinghazem3@gmail.com>
2025-05-29 12:27:03 +0300

    lang

--- src/i18n.js
+++ src/i18n.js
@@ -1,13 +1,13 @@
-import { createI18n } from 'vue-i18n';
+// src/i18n.js
 
-// استيراد ملفات الترجمة
+import { createI18n } from 'vue-i18n';
 import en from './locales/en.json';
 import ar from './locales/ar.json';
 
-// إنشاء نسخة من i18n
 const i18n = createI18n({
-  locale: 'ar', // تحديد اللغة الافتراضية (العربية)
-  fallbackLocale: 'en', // تحديد اللغة البديلة في حال عدم توفر الترجمة
+  legacy: false, // <--- أضف هذا السطر هنا
+  locale: 'ar',
+  fallbackLocale: 'en',
   messages: {
     en,
     ar,
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
+import './style.css'; // أضف هذا السطر
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import router from './router';
-import i18n from './i18n'; // استيراد إعدادات i18n
+import i18n from './i18n';
 import App from './App.vue';
 
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
-app.use(i18n); // استخدام إعدادات i18n
+app.use(i18n);
 app.mount('#app');
